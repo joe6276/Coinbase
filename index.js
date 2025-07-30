267285fa940a3ee44dd5cdbd0518c98967fb3ca8
Jonathan Ndambuki <joendambuki16@gmail.com>
2025-07-30 15:41:24 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,106 @@
+const express = require("express")
+const dotenv = require("dotenv")
+const axios = require("axios")
+
+
+const coinbase = require("coinbase-commerce-node")
+
+dotenv.config()
+
+
+const app = express()
+
+app.use(express.json({
+    verify: (req, res, buf) => {
+        const url = req.originalUrl;
+        if (url.startsWith("/webhook")) {
+            req.rawBody = buf.toString()
+        }
+    }
+}))
+
+
+const Client = coinbase.Client
+const resources = coinbase.resources
+const webhook = coinbase.Webhook
+
+
+Client.init(process.env.COINBASE_API_KEY)
+
+app.post("/checkout", async (req, res) => {
+
+    const { amount, currency, diagnosisId, redirect_url, cancel_url } = req.body
+
+
+    const response = await axios.post("https://docstant-cnhrfqh8fnajc6gj.canadacentral-01.azurewebsites.net/api/Coinbase",
+
+        {
+            "diagnosisId": "29db31f3-ff53-4506-6750-08ddba5952bf",
+            "amount": 1,
+            "currency": "USD"
+        }
+    );
+
+   const id =response.data.toString();
+
+
+    try {
+
+        const charge = await resources.Charge.create({
+            name: "Test Charge",
+            description: "Test Description",
+            local_price: {
+                amount: amount,
+                currency: currency
+            },
+            pricing_type: 'fixed_price',
+            metadata: {
+                user_id: id
+            },
+            redirect_url: redirect_url,
+            cancel_url: cancel_url
+        })
+
+        res.status(200).json({
+            charge: charge
+        })
+    } catch (error) {
+        res.status(500).json({
+            error: error
+        })
+    }
+})
+
+
+app.post("/webhook", express.raw({ type: 'application/json' }), async (req, res) => {
+    try {
+
+        const event = webhook.verifyEventBody(
+            req.rawBody,
+            req.headers["x-cc-webhook-signature"],
+            process.env.COINBASE_WEBHOOK_SECRET
+        )
+
+        if (event.type == "charge:confirmed") {
+
+            let amount = event.data.pricing.local.amount
+            let currency = event.data.pricing.local.currency
+            let userId = event.data.metadata.user_id
+
+            console.log(amount, currency, userId);
+
+            await axios.post(`https://docstant-cnhrfqh8fnajc6gj.canadacentral-01.azurewebsites.net/api/Coinbase/${userId}`);
+        }
+
+        res.sendStatus(200)
+
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+
+app.listen(3000, () => {
+    console.log("Server is Running on 3000");
+
+})
